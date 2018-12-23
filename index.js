9b2351dafea2acb4496990eb3af23eeb8c4dbf59
Harriet Ryder <harriethryder@gmail.com>
2018-12-23 07:05:02 +0000

    Add support for requesting challenges by difficulty

--- index.js
+++ index.js
@@ -6,28 +6,71 @@ const AWS = require('aws-sdk');
  * @param {Object} context
  * @param {Function} callback
  */
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event, context) => {
+  console.log({
+    path: event.path,
+    params: event.pathParameters,
+    query: event.queryStringParameters,
+  });
+
   const dynamodb = new AWS.DynamoDB({
     logger: console,
   });
 
-  try {
-    const res = await dynamodb.scan({
+  let res;
+
+  if (event.queryStringParameters && event.queryStringParameters.difficulty) {
+    const difficulty = event.queryStringParameters.difficulty;
+    if (isNaN(Number(difficulty))) {
+      return {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({messgae: 'Invalid difficulty: must be a number'}),
+      };
+    }
+
+    const query = {
+      TableName: 'bash_challenges',
+      IndexName: 'bash_challenges_by_difficulty',
+      KeyConditionExpression: 'difficulty = :difficulty',
+      ExpressionAttributeValues: {
+        ':difficulty': {
+          N: difficulty,
+        },
+      },
+    };
+
+    console.log('Querying DyamoDB', {query});
+    try {
+      res = await dynamodb.query(query).promise();
+    } catch (e) {
+      console.log('Error', {error: e});
+      throw new Error(e);
+    }
+  } else {
+    const query = {
       TableName: 'bash_challenges',
       ProjectionExpression: 'title, description, id, difficulty',
-    }).promise();
-    console.log(res);
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({challenges: res}),
     };
-    callback(null, response);
-  } catch (e) {
-    console.log(e);
-    callback(e);
+
+    console.log('Querying DyamoDB', {query});
+    try {
+      res = await dynamodb.scan(query).promise();
+    } catch (e) {
+      console.log('Error', {error: e});
+      throw new Error(e);
+    }
   }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({challenges: res.Items}),
+  };
 };
